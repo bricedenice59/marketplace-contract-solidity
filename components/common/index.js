2ebe390ff224aa1b379692b0240f3c7a63c4abed
Brice Grenard <brice.grenard@gmail.com>
2022-06-13 12:04:29 +0800

    Components creation

---
+++ components/common/index.js
@@ -0,0 +1,4 @@
+export { default as FooterComponent } from "./footer"
+export { default as NavbarComponent } from "./navbar"
+export { default as BreadCrumbsComponent } from "./breadcrumbs"
+export { default as HeroComponent } from "./hero"
