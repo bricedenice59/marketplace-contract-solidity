b998070b676229f170d829ae2fa9d25ede59bad2
Brice Grenard <brice.grenard@gmail.com>
2022-07-05 14:30:04 +0800

    Use gas reporter lib

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,19 @@
  */
 
 require("@nomiclabs/hardhat-truffle5");
+require("hardhat-gas-reporter");
+require("dotenv").config();
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 module.exports = {
   solidity: "0.8.14",
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    //token: "BNB",
+  },
 };
