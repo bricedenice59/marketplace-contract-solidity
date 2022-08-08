47656cb1a0ad38fb0f72f2113728b4951c62516c
Brice Grenard <brice.grenard@gmail.com>
2022-08-08 14:07:02 +0800

    Add script to populate contract with course authors and courses; run it with following command: hh run ./populate_contract/populate.js --network goerli

--- content/courses/fetcher.js
+++ content/courses/fetcher.js
@@ -1,25 +1,4 @@
 const courses = require("./index.json");
-const coursesOwners = require("./courseAuthors.json");
-
-const getAllCoursesOwners = () => {
-    return {
-        data: coursesOwners,
-    };
-};
-
-const getAllCoursesOwnersForContractUse = () => {
-    var coursesOwners = [];
-    getAllCoursesOwners().data.forEach((owner) => {
-        coursesOwners.push({
-            id: owner.id,
-            address: owner.address,
-            rewardPercentage: owner.rewardPercentage,
-        });
-    });
-    return {
-        data: coursesOwners,
-    };
-};
 
 const getAllCourses = () => {
     return {
@@ -32,9 +11,6 @@ const getAllParsedCoursesForContractUse = () => {
     getAllCourses().data.forEach((course) => {
         courses.push({
             id: course.id,
-            title: course.title,
-            price: course.price,
-            courseAuthor: course.courseAuthor,
         });
     });
     return {
@@ -44,4 +20,3 @@ const getAllParsedCoursesForContractUse = () => {
 
 module.exports.getAllCourses = getAllCourses;
 module.exports.getAllParsedCoursesForContractUse = getAllParsedCoursesForContractUse;
-module.exports.getAllCoursesOwnersForContractUse = getAllCoursesOwnersForContractUse;
---
+++ populate_contract/populate.js
@@ -0,0 +1,80 @@
+const { getNamedAccounts, network, ethers } = require("hardhat");
+const { contractAddresses } = require("../contracts_constants/index");
+const { getAllParsedCoursesForContractUse } = require("../content/courses/fetcher");
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function main() {
+    var accounts = await ethers.getSigners();
+    const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId;
+
+    console.log("----------------------");
+    console.log("Checking if a list of courses exist...");
+    const courses = getAllParsedCoursesForContractUse();
+
+    if (courses.data.length === 0) {
+        throw new Error(
+            "No course were found, are you sure you have a list of courses available in content/courses/index.json?"
+        );
+    }
+
+    console.log(`Found ${accounts.length} accounts`);
+    for (let i = 0; i < accounts.length; i++) {
+        const address = accounts[i].address;
+        console.log(`Account ${i}: ${address}`);
+    }
+    console.log("Checks done...");
+    console.log("----------------------");
+
+    console.log("----------------------");
+    console.log("Populate marketplace contract");
+    const deployedMarketplace = await ethers.getContractAt(
+        "Marketplace",
+        contractAddresses[chainId][0]
+    );
+
+    console.log("Add authors to marketplace contract...");
+    for (let i = 1; i < accounts.length; i++) {
+        //i = 1 because account 0 is the deployer and we don't want the deployer to addcourse, it will anyway ends up in a reverted tx
+        const address = accounts[i].address;
+        const rewardPercentage = getRandomInt(80, 95);
+        console.log(
+            `Adding course author ${address}; ${rewardPercentage}% funded to his/her account for every sold courses`
+        );
+        const txAddCourseAuthor = await deployedMarketplace.addCourseAuthor(
+            address,
+            rewardPercentage,
+            { from: deployer }
+        );
+        await txAddCourseAuthor.wait(network.config.blockConfirmationsForTransactions);
+        console.log(`Course author ${address} added!`);
+    }
+
+    console.log("Add courses to marketplace contract with randomly chosen course author(s)...");
+    for (let i = 0; i < courses.data.length; i++) {
+        const course = courses.data[i];
+        const courseId = "0x" + course.id;
+        const randomAccount = accounts[getRandomInt(1, accounts.length)];
+        console.log(
+            `Adding course with id= ${courseId}; course author is ${randomAccount.address}`
+        );
+        const txAddCourse = await deployedMarketplace.connect(randomAccount).addCourse(courseId);
+        await txAddCourse.wait(network.config.blockConfirmationsForTransactions);
+        console.log(`Course ${courseId} added!`);
+    }
+
+    console.log("Marketplace populated!");
+    console.log("----------------------");
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
